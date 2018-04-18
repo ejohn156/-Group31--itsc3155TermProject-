dffcf09840b070c5297d1eec19a9d49d79baf162
Tyler Wardle <twardle@uncc.edu>
2018-04-18 22:39:18 +0000

    display user's name if they are signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!, except: [:home, :about, :contact]
+  #before_action :authenticate_user!, except: [:home, :about, :contact]
 
   def home
       return "welcome/index"
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname, :username])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:firstname, :lastname, :username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname, :username, :height_feet, :height_inches, :weight])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:firstname, :lastname, :username, :height_feet, :height_inches, :weight])
   end
 end
--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,5 +1,6 @@
 class ExercisesController < ApplicationController
   before_action :set_exercise, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index]
 
   # GET /exercises
   # GET /exercises.json
