d5f67ca4f8d8080f9adae752d0188bfadb1ef304
”Eric <ejohn156@uncc.edu>
2018-04-29 19:16:14 +0000

    added delete functionality to steps

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -4,9 +4,16 @@ class StepsController < ApplicationController
         @step = @exercise.steps.create(step_params)
         redirect_to exercise_path(@exercise)
     end
-    
+    def destroy
+        @exercise = Exercise.find(params[:exercise_id])
+        @step = @exercise.steps.find(params[:id])
+        @step.destroy
+        redirect_to exercise_path(@exercise)
+    end
     private 
         def step_params
             params.require(:step).permit(:reps, :weight, :notes)
         end
+    
+    
 end
