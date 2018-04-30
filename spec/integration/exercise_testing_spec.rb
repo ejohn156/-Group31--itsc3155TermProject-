4dd860482ef545e5c32c621fc3aba87b162abe5f
Sofia Ali <sali13@uncc.edu>
2018-04-30 02:53:00 +0000

    Deleting an Exercise Test

--- spec/integration/exercise_testing_spec.rb
+++ spec/integration/exercise_testing_spec.rb
@@ -10,4 +10,14 @@ feature "Creating a new Exercise" do
         expect(page).to have_content("Name: TRX Squats")
         expect(page).to have_content("Description: Low knee stress squat")
     end
+feature "Deleting an Exercise" 
+    scnerario "user successfully deletes an exercise"do
+        visit exercises_path
+        expect(page).to have_content("Exercises")
+        click_link "Delete"
+        click_link "Okay"
+        expect(page).to have_no_conten("Name: TRX Squats")
+        expect(page).to have_no_content("Description: Low knee stress squat")
+    end
+
 end
